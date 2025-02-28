9ec7af125ba8eff72e32ce4be57c2ddf25ed9b7e
Taoveeque <taotundex001@gmail.com>
2025-02-28 23:11:39 +0100

    new update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -11,7 +11,7 @@ const page = () => {
                     <Image className='mx-auto' src="/images/Male User.png" width={50} height={50} alt="logo 1" />
                     <h3 className='text-[36px] text-center font-bold'>Login</h3>
                     {/* <p className='font-medium text-[18px]'>Enter your email and password access your dashboard</p> */}
-                    <form action="" method="post" className='font-medium text-[16px] pt-10 pb-5 flex flex-col gap-[20px]'>
+                    <form action="/pages/dashboard" method="post" className='font-medium text-[16px] pt-10 pb-5 flex flex-col gap-[20px]'>
                         <label className="form-control flex flex-col gap-1 w-full">Email
                             <input type="email" placeholder="e. g. johndoe@gmail.com" name="email" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
                         </label>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
                     <Image className='mx-auto' src="/images/BV 1 Logo 1.png" width={200} height={50} alt="logo 1" />
                     <h3 className='text-[36px] text-center font-bold'>Sign up your account</h3>
                     <p className='font-medium text-[18px]'>Enter your personal data to create an account</p>
-                    <form action="" method="post" className='grid grid-cols-2 gap-x-[50px] gap-y-[20px] font-medium text-[16px] pt-10 pb-5'>
+                    <form action="/pages/dashboard" method="post" className='grid grid-cols-2 gap-x-[50px] gap-y-[20px] font-medium text-[16px] pt-10 pb-5'>
                         <label className="form-control flex flex-col gap-1 w-full col-span-1">First name
                             <input type="text" placeholder="e. g. John" name="firstname" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
                         </label>
