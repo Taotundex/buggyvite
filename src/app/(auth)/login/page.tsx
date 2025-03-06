f87568b900c831ca69b5667c2856c3fd2a3f3835
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2025-03-06 17:43:38 +0100

    Installed neccessary packages like formik and yup to handle form validation or basically everything needed to process the apis Having issue importing my api url in the dotenv into my component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,53 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from "yup"
+import axios from 'axios';
+// import { useNavigate } from 'react-router-dom'
+
+interface LoginFormValues {
+	email: string;
+	password: string;
+}
 
 const page = () => {
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    // const navigate = useNavigate()
+
+    const formik = useFormik<LoginFormValues>({
+        initialValues:{
+            email: "",
+            password: "",
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email("Invalid Email Address")
+                .required("Email is required!."),
+            password: Yup.string()
+                .required("Password is required!.")
+                .min(8, "Password must be at least 8 characters")
+        }),
+        onSubmit: async (values, { setSubmitting, resetForm }) => {
+            setIsLoading(true)
+            setSubmitting(true)
+            try {
+                // const response = await axios.post("/login", values)
+                // navigate("/pages/dashboard")
+            } catch (error: any) {
+                
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    setSubmitting(false);
+                    resetForm();
+                }, 5000);
+            }
+        }
+    })
+
     return (
         <div className='h-screen'>
             <div className="container grid lg:grid-cols-2 grid-cols-1 gap-[75px] h-full items-center">
@@ -11,15 +56,43 @@ const page = () => {
                     <Image className='mx-auto' src="/images/Male User.png" width={50} height={50} alt="logo 1" />
                     <h3 className='text-[36px] text-center font-bold'>Login</h3>
                     {/* <p className='font-medium text-[18px]'>Enter your email and password access your dashboard</p> */}
-                    <form action="/pages/dashboard" method="post" className='font-medium text-[16px] pt-10 pb-5 flex flex-col gap-[20px]'>
+                    <form action="/pages/dashboard" method="post" className='font-medium text-[16px] pt-10 pb-5 flex flex-col gap-[20px]' onSubmit={formik.handleSubmit}>
                         <label className="form-control flex flex-col gap-1 w-full">Email
-                            <input type="email" placeholder="e. g. johndoe@gmail.com" name="email" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
+                            <input 
+                                type="email" 
+                                placeholder="e. g. johndoe@gmail.com" 
+                                name="email" 
+                                className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" 
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.email && formik.errors.email && (
+								<small className="text-red-500 text-[12px]">{formik.errors.email}</small>
+							)}
                         </label>
                         <label className="form-control flex flex-col gap-1 w-full">Password
-                            <input type="password" placeholder="********" minLength={8} name="password" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
+                            <input 
+                                type="password" 
+                                placeholder="********" 
+                                minLength={8} 
+                                name="password" 
+                                className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" 
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.password && formik.errors.password && (
+								<small className="text-red-500 text-[12px]">{formik.errors.password}</small>
+							)}
                         </label>
                         <Link href="forget_password" className='text-[16px] w-full flex justify-end mt-[-15px] underline underline-offset-2'>Forgot Password?</Link>
-                        <button className="btn col-span-2 w-full bg-[#796FAB] border-none text-[#D9D9D9]"><Link href="/pages/dashboard">Sign in</Link></button>
+                        <button 
+                            className={`btn col-span-2 w-full bg-[#796FAB] border-none text-[#D9D9D9] ${formik.isSubmitting ? "cursor-not-allowed" : null}`}
+                            disabled={formik.isSubmitting}
+                        >
+                            {
+                                formik.isSubmitting ? "Logging in..." : "Log In"
+                            }
+                        </button>
                     </form>
                     <center className="text-[16px] font-medium">Don’t have an account? <Link href="signup" className='text-[#796FAB] underline underline-offset-2'>Sign up</Link></center>
                 </div>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,62 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from "yup"
+import axios from 'axios';
+// import { useNavigate } from 'react-router-dom'
+
+interface SignUpFormValues {
+	firstname: string;
+	lastname: string;
+	email: string;
+	password: string;
+}
+
 
 const page = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    // const navigate = useNavigate()
+
+    const formik = useFormik<SignUpFormValues>({
+        initialValues:{
+            firstname: "",
+            lastname: "",
+            email: "",
+            password: "",
+        },
+        validationSchema : Yup.object({
+            firstname: Yup.string()
+                .required("First Name is required!."),
+            lastname: Yup.string()
+                .required("Last Name is required!."),
+            email: Yup.string()
+                .email("Invalid email address")
+                .required("Email is required"),
+            password: Yup.string()
+                .required("Password is required")
+                .min(8, "Password must be at least 8 characters"),
+
+        }),
+        onSubmit: async (values, { setSubmitting, resetForm }) => {
+            setIsLoading(true)
+            setSubmitting(true)
+            try {
+                // const response = await axios.post("/signup", values)
+                // navigate("/login")
+            } catch (error: any) {
+                
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    setSubmitting(false);
+                    resetForm();
+                }, 3000);
+            }
+        }
+    })
+
     return (
         <div className='h-screen'>
             <div className="container grid lg:grid-cols-2 grid-cols-1 gap-[75px] h-full items-center">
@@ -10,18 +64,59 @@ const page = () => {
                     <Image className='mx-auto' src="/images/BV 1 Logo 1.png" width={200} height={50} alt="logo 1" />
                     <h3 className='text-[36px] text-center font-bold'>Sign up your account</h3>
                     <p className='font-medium text-[18px]'>Enter your personal data to create an account</p>
-                    <form action="/pages/dashboard" method="post" className='grid grid-cols-2 gap-x-[50px] gap-y-[20px] font-medium text-[16px] pt-10 pb-5'>
+                    <form action="/pages/dashboard" method="post" className='grid grid-cols-2 gap-x-[50px] gap-y-[20px] font-medium text-[16px] pt-10 pb-5' onSubmit={formik.handleSubmit}>
                         <label className="form-control flex flex-col gap-1 w-full col-span-1">First name
-                            <input type="text" placeholder="e. g. John" name="firstname" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
+                            <input 
+                                type="text" 
+                                placeholder="e. g. John" 
+                                name="firstname" 
+                                className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" 
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            {formik.touched.firstname && formik.errors.firstname && (
+								<small className="text-red-500 text-[12px]">{formik.errors.firstname}</small>
+							)}
                         </label>
                         <label className="form-control flex flex-col gap-1 w-full col-span-1">Last name
-                            <input type="text" placeholder="e. g. Doe" name="lastname" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
+                            <input 
+                                type="text" 
+                                placeholder="e. g. Doe" 
+                                name="lastname" 
+                                className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" 
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            {formik.touched.lastname && formik.errors.lastname && (
+								<small className="text-red-500 text-[12px]">{formik.errors.lastname}</small>
+							)}
                         </label>
                         <label className="form-control flex flex-col gap-1 w-full col-span-2">Email
-                            <input type="email" placeholder="e. g. johndoe@gmail.com" name="email" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
+                            <input 
+                                type="email" 
+                                placeholder="e. g. johndoe@gmail.com" 
+                                name="email" 
+                                className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" 
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            {formik.touched.email && formik.errors.email && (
+								<small className="text-red-500 text-[12px]">{formik.errors.email}</small>
+							)}
                         </label>
                         <label className="form-control flex flex-col gap-1 w-full col-span-2">Password
-                            <input type="password" placeholder="********" minLength={8} name="password" className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" />
+                            <input 
+                                type="password" 
+                                placeholder="********" 
+                                minLength={8} 
+                                name="password" 
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                                className="input input-bordered w-full bg-transparent border border-[#8E8E8E] rounded-[10px]" 
+                            />
+                            {formik.touched.password && formik.errors.password && (
+                                <small className="text-red-500 text-[12px]">{formik.errors.password}</small>
+                            )}
                             <small className='text-[12px]'>Must be at least 8 characters</small>
                         </label>
                         <div className="flex flex-col col-span-2 gap-2">
@@ -34,7 +129,14 @@ const page = () => {
                                 <span className='text-[#8E8E8E] text-sm w-full'>This form is protected by feCAPTCHA and the Google <Link href="" className='text-black underline underline-offset-2'>Privacy Policy</Link> and <Link href="" className='text-black underline underline-offset-2'>Terms of Service</Link> apply</span>
                             </div>
                         </div>
-                        <button className="btn col-span-2 w-full bg-[#796FAB] border-none text-[#D9D9D9]"><Link href="/pages/dashboard">Sign up</Link></button>
+                        <button 
+                            className={`btn col-span-2 w-full bg-[#796FAB] border-none text-[#D9D9D9] ${formik.isSubmitting ? "cursor-not-allowed" : null}`}
+                            disabled={formik.isSubmitting}
+                        >
+                            {
+                                formik.isSubmitting ? "Signing up..." : "Sign up"
+                            }
+                        </button>
                     </form>
                     <center className="text-[16px] font-medium">Have an account? <Link href="login" className='text-[#796FAB] underline underline-offset-2'>Log in</Link></center>
                 </div>
